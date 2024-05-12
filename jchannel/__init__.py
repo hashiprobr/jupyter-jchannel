0da1368c511e057470505819c7e35f580a0a4204
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2024-05-12 14:36:34 -0300

    Refactor main module

--- jchannel/__init__.py
+++ jchannel/__init__.py
@@ -1,7 +1,17 @@
+import asyncio
+import logging
+
 from jchannel.server import Server
 
 
 def start(host='localhost', port=8889, url=None, heartbeat=30):
     server = Server(host, port, url, heartbeat)
-    server.start()
+    asyncio.create_task(_start(server))
     return server
+
+
+async def _start(server):
+    try:
+        await server._start()
+    except Exception:
+        logging.exception('Server start exception')
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,3 +1,6 @@
+import asyncio
+import logging
+import pytest
 import jchannel
 
 from unittest.mock import Mock
@@ -9,14 +12,22 @@ URL = 'b'
 HEARTBEAT = 1
 
 
-def test_starts(mocker):
-    mock = Mock()
+pytestmark = pytest.mark.asyncio(scope='module')
 
-    Server = mocker.patch('jchannel.Server')
-    Server.return_value = mock
 
-    server = jchannel.start(HOST, PORT, URL, HEARTBEAT)
-    assert server is mock
+async def test_starts(mocker, caplog):
+    with caplog.at_level(logging.ERROR):
+        async def side_effect():
+            raise Exception()
 
-    Server.assert_called_once_with(HOST, PORT, URL, HEARTBEAT)
-    mock.start.assert_called_once()
+        server = Mock()
+        server._start.side_effect = side_effect
+
+        Server = mocker.patch('jchannel.Server')
+        Server.return_value = server
+
+        assert jchannel.start(HOST, PORT, URL, HEARTBEAT) is server
+        Server.assert_called_once_with(HOST, PORT, URL, HEARTBEAT)
+
+        await asyncio.sleep(0)
+    assert len(caplog.records) == 1
