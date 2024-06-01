b45a5106c7039098f9da704d192c09cc134b04ac
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2024-06-01 10:22:26 -0300

    Refactor AbstractServer class

--- jchannel/types.py
+++ jchannel/types.py
@@ -10,7 +10,7 @@ class JavascriptError(Exception):
 
 
 class AbstractServer(ABC):
-    _channels: dict
+    _channels = {}
 
     @abstractmethod
     async def _send(self, body_type, input, channel_key, timeout):
