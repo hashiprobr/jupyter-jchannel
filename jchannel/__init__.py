0c7e556bbaa09e38901402824edcadd8b2f7bb64
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2024-06-25 05:50:39 -0300

    Make main start function asynchronous

--- jchannel/__init__.py
+++ jchannel/__init__.py
@@ -1,17 +1,7 @@
-import asyncio
-import logging
-
 from jchannel.server import Server
 
 
-def start(host='localhost', port=8889, url=None, heartbeat=30):
+async def start(host='localhost', port=8889, url=None, heartbeat=30):
     server = Server(host, port, url, heartbeat)
-    asyncio.create_task(_start(server))
+    await server._start()
     return server
-
-
-async def _start(server):
-    try:
-        await server._start()
-    except:
-        logging.exception('Could not start server')
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,9 +1,7 @@
-import asyncio
-import logging
 import pytest
 import jchannel
 
-from unittest.mock import Mock
+from unittest.mock import AsyncMock
 
 pytestmark = pytest.mark.asyncio(scope='module')
 
@@ -14,15 +12,10 @@ URL = 'http://localhost:8889'
 HEARTBEAT = 3
 
 
-def mock_server(mocker, error):
-    async def instance_side_effect():
-        if error:
-            raise Exception
+async def test_starts(mocker):
+    server = AsyncMock()
 
-    server = Mock()
-    server._start.side_effect = instance_side_effect
-
-    def constructor_side_effect(host, port, url, heartbeat):
+    def side_effect(host, port, url, heartbeat):
         assert host == HOST
         assert port == PORT
         assert url == URL
@@ -30,24 +23,7 @@ def mock_server(mocker, error):
         return server
 
     Server = mocker.patch('jchannel.Server')
-    Server.side_effect = constructor_side_effect
+    Server.side_effect = side_effect
 
-    return server
-
-
-def start():
-    return jchannel.start(HOST, PORT, URL, HEARTBEAT)
-
-
-async def test_starts(mocker):
-    server = mock_server(mocker, False)
-    assert start() is server
-    await asyncio.sleep(0)
-
-
-async def test_does_not_start(caplog, mocker):
-    with caplog.at_level(logging.ERROR):
-        server = mock_server(mocker, True)
-        assert start() is server
-        await asyncio.sleep(0)
-    assert len(caplog.records) == 1
+    assert await jchannel.start(HOST, PORT, URL, HEARTBEAT) is server
+    server._start.assert_awaited_once()
